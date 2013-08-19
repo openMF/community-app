ee57412bca193b4086ce7f780a4850e87dccfb9a
Nayan Ambali <nayan.ambali@gmail.com>
2013-08-19 15:53:13 +0530

    added other langs

--- js/controllers/UserSettingController.js
+++ js/controllers/UserSettingController.js
@@ -1,11 +1,26 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
     UserSettingController: function(scope, translate) {
-      scope.changeLanguage = function (langKey) {
-        translate.uses(langKey);
+      
+      
+
+      scope.langs   = [{"name" : "English" , "code" : "en"},
+                       {"name" : "French", "code":"fr"},
+                       {"name" : "Spanish", "code":"es"},
+                       {"name" : "Portuguese", "code":"pt"},
+                       {"name" : "Chinese", "code":"zh"},  
+                       ];
+
+      scope.optlang = scope.langs[0];                       
+
+      scope.changeLang = function (lang) {
+          translate.uses(lang.code);
+          scope.optlang = lang;
       };
+
     }
   });
+
   mifosX.ng.application.controller('UserSettingController', ['$scope', '$translate', mifosX.controllers.UserSettingController]).run(function($log) {
     $log.info("UserSettingController initialized");
   });
