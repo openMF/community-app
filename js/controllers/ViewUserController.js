714b02de2f8e91de2b9819537c2d8ad2cc216e91
Nayan Ambali <nayan.ambali@gmail.com>
2013-08-20 10:47:06 +0530

    Delete user

--- js/controllers/ViewUserController.js
+++ js/controllers/ViewUserController.js
@@ -1,13 +1,21 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
-    ViewUserController: function(scope, routeParams , resourceFactory ) {
+    ViewUserController: function(scope, routeParams, location, resourceFactory ) {
         scope.user = [];
         resourceFactory.userListResource.get({userId: routeParams.id} , function(data) {
             scope.user = data;
         });
+
+        scope.deleteuser = function (){
+          resourceFactory.userListResource.delete({userId: routeParams.id} , {} , function(data) {
+                location.path('/userslist');
+                // added dummy request param because Content-Type header gets removed 
+                // if the request does not contain any data (a request body)        
+          });
+        };
     }
   });
-  mifosX.ng.application.controller('ViewUserController', ['$scope', '$routeParams','ResourceFactory', mifosX.controllers.ViewUserController]).run(function($log) {
+  mifosX.ng.application.controller('ViewUserController', ['$scope', '$routeParams', '$location', 'ResourceFactory', mifosX.controllers.ViewUserController]).run(function($log) {
     $log.info("ViewUserController initialized");
   });
 }(mifosX.controllers || {}));
