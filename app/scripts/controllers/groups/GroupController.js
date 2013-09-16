4418304bd8758746ba25a95f06aa4f1ac7364330
safiyu <dheensafiyu@gmail.com>
2013-09-16 17:43:27 +0530

    groups

---
+++ app/scripts/controllers/groups/GroupController.js
@@ -0,0 +1,19 @@
+(function(module) {
+    mifosX.controllers = _.extend(module, {
+        GroupController: function(scope, resourceFactory , paginatorService) {
+
+            scope.groups = [];
+
+            var fetchFunction = function(offset, limit, callback) {
+                resourceFactory.groupResource.getAllGroups({offset: offset, limit: limit} , callback);
+            };
+
+            scope.groups = paginatorService.paginate(fetchFunction, 14);
+
+        }
+    });
+    mifosX.ng.application.controller('GroupController', ['$scope', 'ResourceFactory', 'PaginatorService', mifosX.controllers.GroupController]).run(function($log) {
+        $log.info("GroupController initialized");
+    });
+}(mifosX.controllers || {}));
+
