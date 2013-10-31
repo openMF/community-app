3dfe23788bed781efd36d3b03a21ebf1b15ec88e
goutham-M <goutham@confluxtechnologies.com>
2013-10-31 16:30:26 +0530

    client side validation is added to group

--- app/scripts/controllers/groups/AddRoleController.js
+++ app/scripts/controllers/groups/AddRoleController.js
@@ -1,10 +1,13 @@
 (function(module) {
     mifosX.controllers = _.extend(module, {
         AddRoleController: function(scope, routeParams , location, resourceFactory) {
+            scope.formData = {};
             resourceFactory.groupResource.get({groupId: routeParams.id,associations:'all',template:'true'} , function(data) {
                 scope.group = data;
                 scope.clients = data.clientMembers;
                 scope.roles = data.availableRoles;
+                scope.formData.clientId = data.clientMembers[0].id;
+                scope.formData.role = data.availableRoles[0].id;
             });
 
             scope.addrole = function(){
