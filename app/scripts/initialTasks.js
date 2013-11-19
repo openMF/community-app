3eac16b8cd5e11763260bd0a78b2d57e5a807f9f
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-11-19 15:46:49 +0530

    Cleanup whitespace and conditions
    Done with the help of JSHint ( http://www.jshint.com ).

--- app/scripts/initialTasks.js
+++ app/scripts/initialTasks.js
@@ -1,5 +1,14 @@
 (function(mifosX) {
-  var defineHeaders = function($httpProvider , $translateProvider, ResourceFactoryProvider ,HttpServiceProvider,TENANT ,CONTENT_TYPE , HOST, API_URL_OVERRIDE ) {
+  var defineHeaders = function(
+    $httpProvider,
+    $translateProvider,
+    ResourceFactoryProvider,
+    HttpServiceProvider,
+    TENANT,
+    CONTENT_TYPE,
+    HOST,
+    API_URL_OVERRIDE
+  ) {
 
     // Fix API URL?
     if (API_URL_OVERRIDE  === 'true') {
@@ -10,44 +19,42 @@
 
         $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = TENANT;
 
-     }
-     else{
+     } else {
           // For multi tenant hosting
           var hostname = window.location.hostname;
+
           console.log('hostname---'+hostname);
           domains = hostname.split('.');
           console.log('domains---'+domains);
-          if(domains[0] == "demo"){
+          if(domains[0] == "demo") {
                   $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = TENANT;
                   console.log("demo server",domains[0]);
-          }
-          else{
+          } else {
                   $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = domains[0];
                   console.log("other than demo server", domains[0]);
-
           }
-     }   
+     }
 
-	// Enable CORS! (see e.g. http://enable-cors.org/) 
-	$httpProvider.defaults.useXDomain = true;
-	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    // Enable CORS! (see e.g. http://enable-cors.org/)
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
-  	//Set headers
+    //Set headers
     $httpProvider.defaults.headers.common['Content-Type'] = CONTENT_TYPE;
 
     // Configure i18n and preffer language
- 	  //$translateProvider.translations('en', translationsEN);
-  	//$translateProvider.translations('de', translationsDE);
+    //$translateProvider.translations('en', translationsEN);
+    //$translateProvider.translations('de', translationsDE);
 
     $translateProvider.useStaticFilesLoader({
           prefix: 'global-translations/locale-',
           suffix: '.json'
     });
 
-  	$translateProvider.preferredLanguage('en');
-  	$translateProvider.fallbackLanguage('en');
-
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.fallbackLanguage('en');
   };
+
   mifosX.ng.application.config(defineHeaders).run(function($log) {
     $log.info("Initial tasks are done!");
   });
