a45f2fddcf2c323d7d39e58907deb7d3323e1980
Safiyu <dheensafiyu@gmail.com>
2013-11-19 10:57:48 +0530

    fixed issue regarding to codes

--- app/scripts/controllers/system/CodeController.js
+++ app/scripts/controllers/system/CodeController.js
@@ -1,13 +1,16 @@
 (function(module) {
     mifosX.controllers = _.extend(module, {
-        CodeController: function(scope, resourceFactory) {
+        CodeController: function(scope, resourceFactory,location) {
             scope.codes = [];
             resourceFactory.codeResources.getAllCodes(function(data){
                 scope.codes = data;
-        });
+            });
+            scope.routeTo = function(id){
+                location.path('/viewcode/'+id);
+            }
        }
      });
-    mifosX.ng.application.controller('CodeController', ['$scope', 'ResourceFactory', mifosX.controllers.CodeController]).run(function($log) {
+    mifosX.ng.application.controller('CodeController', ['$scope', 'ResourceFactory','$location', mifosX.controllers.CodeController]).run(function($log) {
         $log.info("CodeController initialized");
     });
 }(mifosX.controllers || {}));
