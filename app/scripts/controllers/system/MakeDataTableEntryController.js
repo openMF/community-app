98a6338f7b7aee0164fa493baa70e70cdb180034
goutham-M <goutham@confluxtechnologies.com>
2013-11-29 21:25:12 +0530

    datatable issue is fixed

--- app/scripts/controllers/system/MakeDataTableEntryController.js
+++ app/scripts/controllers/system/MakeDataTableEntryController.js
@@ -1,6 +1,6 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
-    MakeDataTableEntryController: function(scope, location, routeParams, resourceFactory) {
+    MakeDataTableEntryController: function(scope, location, routeParams, resourceFactory, dateFilter) {
       scope.tableName = routeParams.tableName;
       scope.entityId = routeParams.entityId;
       scope.columnHeaders = [];
@@ -17,12 +17,36 @@
           scope.isCenter = colName ==  'center_id' ? true : false;
         }
         scope.columnHeaders = data.columnHeaders;
+
+        //below logic helps if column name contains space then underscore will be  appended.
+        for (var i=0; i < scope.columnHeaders.length; i++) {
+            scope.columnHeaders[i].columnName = spaceToUnderscore(scope.columnHeaders[i].columnName);
+        }
       });
 
+      //this function removes white space and add underscore
+      var spaceToUnderscore = function(str) {
+          return str.replace(/ /g, "_")
+      }
+
+      scope.cancel = function () {
+          location.path('/viewclient/'+ routeParams.entityId);
+      };
       scope.submit = function () {
         var params = {datatablename:scope.tableName, entityId:scope.entityId, genericResultSet: 'true'};
         this.formData.locale = 'en';
         this.formData.dateFormat =  'dd MMMM yyyy';
+        //below logic, for the input field if data is not entered, this logic will put "", because
+        //if no data entered in input field , that field name won't send to server side.
+        for (var i=0; i < scope.columnHeaders.length; i++) {
+            if (!_.contains(_.keys(this.formData), scope.columnHeaders[i].columnName)) {
+                this.formData[scope.columnHeaders[i].columnName] = "";
+            } else if (scope.columnHeaders[i].columnDisplayType == 'DATE') {
+                this.formData[scope.columnHeaders[i].columnName] = dateFilter(this.formData[scope.columnHeaders[i].columnName],'dd MMMM yyyy');
+            }
+        }
+
+
         resourceFactory.DataTablesResource.save(params, this.formData, function(data){
           var destination = "";
           if ( data.loanId) {
@@ -46,7 +70,7 @@
 
     }
   });
-  mifosX.ng.application.controller('MakeDataTableEntryController', ['$scope', '$location', '$routeParams', 'ResourceFactory', mifosX.controllers.MakeDataTableEntryController]).run(function($log) {
+  mifosX.ng.application.controller('MakeDataTableEntryController', ['$scope', '$location', '$routeParams', 'ResourceFactory', 'dateFilter', mifosX.controllers.MakeDataTableEntryController]).run(function($log) {
     $log.info("MakeDataTableEntryController initialized");
   });
 }(mifosX.controllers || {}));
