21a5113064033ad66a5524a6f80621f58cdaa979
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2013-12-11 11:22:05 -0600

    removed comments from failing tests

--- test/spec/controllers/LoginFormControllerSpec.js
+++ test/spec/controllers/LoginFormControllerSpec.js
@@ -1,32 +1,32 @@
 describe("LoginFormController", function() {
-//  var eventListener;
-//  beforeEach(function() {
-//    this.scope = jasmine.createSpyObj("$scope", ['$on']);
-//    this.scope.$on.andCallFake(function(event, listener) {
-//      eventListener = listener;
-//    });
-//    this.authenticationService = jasmine.createSpyObj("AuthenticationService", ['$on']);
-//
-//    this.controller = new mifosX.controllers.LoginFormController(this.scope, this.authenticationService);
-//  });
-//
-//  it("should initialise the login credentials", function() {
-//    expect(this.scope.loginCredentials).toEqual({});
-//  });
-//
-//  it("should initialise the authenticationFailed flag", function() {
-//    expect(this.scope.authenticationFailed).toBeFalsy();
-//  });
-//
-//  it("should listen to 'UserAuthenticationFailureEvent'", function() {
-//    expect(this.scope.$on).toHaveBeenCalledWith("UserAuthenticationFailureEvent", jasmine.any(Function));
-//  });
-//
-//  describe("on receving 'UserAuthenticationFailureEvent'", function() {
-//    it("should set the authenticationFailed flag to true", function() {
-//      eventListener({});
-//
-//      expect(this.scope.authenticationFailed).toBeTruthy();
-//    });
-//  });
+  var eventListener;
+  beforeEach(function() {
+    this.scope = jasmine.createSpyObj("$scope", ['$on']);
+    this.scope.$on.andCallFake(function(event, listener) {
+      eventListener = listener;
+    });
+    this.authenticationService = jasmine.createSpyObj("AuthenticationService", ['$on']);
+
+    this.controller = new mifosX.controllers.LoginFormController(this.scope, this.authenticationService);
+  });
+
+  it("should initialise the login credentials", function() {
+    expect(this.scope.loginCredentials).toEqual({});
+  });
+
+  it("should initialise the authenticationFailed flag", function() {
+    expect(this.scope.authenticationFailed).toBeFalsy();
+  });
+
+  it("should listen to 'UserAuthenticationFailureEvent'", function() {
+    expect(this.scope.$on).toHaveBeenCalledWith("UserAuthenticationFailureEvent", jasmine.any(Function));
+  });
+
+  describe("on receving 'UserAuthenticationFailureEvent'", function() {
+    it("should set the authenticationFailed flag to true", function() {
+      eventListener({});
+
+      expect(this.scope.authenticationFailed).toBeTruthy();
+    });
+  });
 });
--- test/spec/services/ResourceFactoryProviderSpec.js
+++ test/spec/services/ResourceFactoryProviderSpec.js
@@ -1,38 +1,38 @@
 describe("ResourceFactoryProvider", function() {
-//  var ngResource,
-//      apiVersion = "/mifosng-provider/api/v1";
-//  beforeEach(function() {
-//    this.provider = new mifosX.services.ResourceFactoryProvider();
-//    ngResource = jasmine.createSpy("$resource").andReturn("test_resource");
-//
-//    this.factory = this.provider.$get[1](ngResource);
-//  });
-//
-//  describe("User resource", function() {
-//    it("should define the User resource", function() {
-//      expect(ngResource).toHaveBeenCalledWith(apiVersion + "/users/:userId", {}, {
-//        getAllUsers: {method: 'GET', params: {fields: "id,firstname,lastname,username,officeName"}, isArray: true}
-//      });
-//      expect(this.factory.userResource).toEqual("test_resource");
-//    });
-//  });
-//
-//  describe("Role resource", function() {
-//    it("should define the Role resource", function() {
-//      expect(ngResource).toHaveBeenCalledWith(apiVersion + "/roles/:roleId", {}, {
-//        getAllRoles: {method: 'GET', params: {}, isArray: true}
-//      });
-//      expect(this.factory.roleResource).toEqual("test_resource");
-//    });
-//  });
-//
-//  describe("Office resource", function() {
-//    it("should define the Office resource", function() {
-//      expect(ngResource).toHaveBeenCalledWith(apiVersion + "/offices/:officeId", {officeId:"@officeId"}, {
-//        getAllOffices: {method: 'GET', params: {}, isArray: true},
-//        update: {method: 'PUT'}
-//      });
-//      expect(this.factory.officeResource).toEqual("test_resource");
-//    });
-//  });
+  var ngResource,
+      apiVersion = "/mifosng-provider/api/v1";
+  beforeEach(function() {
+    this.provider = new mifosX.services.ResourceFactoryProvider();
+    ngResource = jasmine.createSpy("$resource").andReturn("test_resource");
+
+    this.factory = this.provider.$get[1](ngResource);
+  });
+
+  describe("User resource", function() {
+    it("should define the User resource", function() {
+      expect(ngResource).toHaveBeenCalledWith(apiVersion + "/users/:userId", {}, {
+        getAllUsers: {method: 'GET', params: {fields: "id,firstname,lastname,username,officeName"}, isArray: true}
+      });
+      expect(this.factory.userResource).toEqual("test_resource");
+    });
+  });
+
+  describe("Role resource", function() {
+    it("should define the Role resource", function() {
+      expect(ngResource).toHaveBeenCalledWith(apiVersion + "/roles/:roleId", {}, {
+        getAllRoles: {method: 'GET', params: {}, isArray: true}
+      });
+      expect(this.factory.roleResource).toEqual("test_resource");
+    });
+  });
+
+  describe("Office resource", function() {
+    it("should define the Office resource", function() {
+      expect(ngResource).toHaveBeenCalledWith(apiVersion + "/offices/:officeId", {officeId:"@officeId"}, {
+        getAllOffices: {method: 'GET', params: {}, isArray: true},
+        update: {method: 'PUT'}
+      });
+      expect(this.factory.officeResource).toEqual("test_resource");
+    });
+  });
 });
