2ccacb5f44fbf207062fddde8b1190f0c26b55f1
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2013-12-11 16:09:25 -0600

    added other relative paths

--- karma.conf.js
+++ karma.conf.js
@@ -8,15 +8,15 @@ module.exports = function (config) {
 
         files : [
         './app/bower_components/angular/angular.js',
-        'app/bower_components/angular-mocks/angular-mocks.js',
-        'app/bower_components/underscore/underscore-min.js',
-        'app/bower_components/requirejs/require.js',
-        'app/scripts/mifosX.js',
-        'app/scripts/models/*.js',
-        'app/scripts/services/*.js',
-        'app/scripts/directives/*.js',
-        'app/scripts/controllers/**/*.js',
-        'test/spec/**/*.js'
+        './app/bower_components/angular-mocks/angular-mocks.js',
+        './app/bower_components/underscore/underscore-min.js',
+        './app/bower_components/requirejs/require.js',
+        './app/scripts/mifosX.js',
+        './app/scripts/models/*.js',
+        './app/scripts/services/*.js',
+        './app/scripts/directives/*.js',
+        './app/scripts/controllers/**/*.js',
+        './test/spec/**/*.js'
         ],
 
         // list of files to exclude
