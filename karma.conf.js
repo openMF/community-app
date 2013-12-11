3051ef314f6238b70cf0b9c8dfd196b057149d75
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2013-12-11 15:32:20 -0600

    fixing file relative path

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
         frameworks : ["jasmine"],
 
         files : [
-        'app/bower_components/angular/angular.js',
+        './app/bower_components/angular/angular.js',
         'app/bower_components/angular-mocks/angular-mocks.js',
         'app/bower_components/underscore/underscore-min.js',
         'app/bower_components/requirejs/require.js',
