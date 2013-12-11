b772de0e09b1f14992cd5753e2847c0f11ceb3e7
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2013-12-11 11:03:20 -0600

    tests runnning and failing how we want

--- karma.conf.js
+++ karma.conf.js
@@ -4,14 +4,34 @@ module.exports = function (config) {
     config.set({
         basePath : '',
 
-        // Fix for "JASMINE is not supported anymore" warning
-        frameworks : ["jasmine", "requirejs"],
+        frameworks : ["jasmine"],
 
         files : [
         'app/bower_components/angular/angular.js',
         'app/bower_components/angular-mocks/angular-mocks.js',
-        'app/scripts/loader.js',
-//        'app/scripts/*.js',
+        'app/bower_components/underscore/underscore-min.js',
+        'app/bower_components/requirejs/require.js',
+        'app/scripts/mifosX.js',
+        'app/scripts/models/roleMap.js',
+        'app/scripts/models/LoggedInUser.js',
+        'app/scripts/models/Role.js',
+        'app/scripts/models/Langs.js',
+        'app/scripts/services/AuthenticationService.js',
+        'app/scripts/services/SessionManager.js',
+        'app/scripts/services/HttpServiceProvider.js',
+        'app/scripts/services/ResourceFactoryProvider.js',
+        'app/scripts/directives/DataTablesDirective.js',
+        'app/scripts/directives/OverlayDirective.js',
+        'app/scripts/directives/DialogDirective.js',
+        'app/scripts/controllers/main/MainController.js',
+        'app/scripts/controllers/main/LoginFormController.js',
+        'app/scripts/controllers/organization/RoleController.js',
+        'app/scripts/controllers/user/UserController.js',
+        'app/scripts/controllers/user/UserFormController.js',
+        'app/scripts/controllers/user/UserListController.js',
+        'app/scripts/controllers/client/ClientController.js',
+        'app/scripts/controllers/user/UserSettingController.js',
+        'app/scripts/controllers/main/SearchController.js',
 //        'app/scripts/**/*.js',
         'test/spec/**/*.js'
         ],
@@ -39,7 +59,7 @@ module.exports = function (config) {
 
         autoWatch : false,
 
-        browsers : ['Chrome'],
+        browsers : ['PhantomJS'],
 
         // If browser does not capture in given timeout [ms], kill it
         captureTimeout : 5000,
