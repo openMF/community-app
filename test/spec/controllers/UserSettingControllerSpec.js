6179e673e903a54eb223d7140062b2203ec0d7d1
SWatro <Thoughtworker@SW9640.local>
2013-12-17 15:05:16 -0600

    fixes tests in usersettingcontrollerspec

--- test/spec/controllers/UserSettingControllerSpec.js
+++ test/spec/controllers/UserSettingControllerSpec.js
@@ -1,28 +1,28 @@
-describe("UserSettingController", inject(function() {
+describe("UserSettingController", function() {
 
     beforeEach(function() {
         this.scope = {};
         this.translate = {
-            uses: jasmine.createSpy("translate.uses('data')")
+            uses: jasmine.createSpy("translate.uses()")
         };
         this.localStorageService = {
-            get: jasmine.createSpy("localStorageService.get('Language')").andReturn({"name" : "English" , "code" : "en"}),
-            add: jasmine.createSpy("localStorageService.add('Language',data)")
+            get: jasmine.createSpy("localStorageService.get()").andReturn({"name" : "English" , "code" : "en"}),
+            add: jasmine.createSpy("localStorageService.add()")
         };
         this.controller = new mifosX.controllers.UserSettingController(this.scope, this.translate,this.localStorageService);
     });
 
 
-    it("should populate the scope with available languages", inject(function() {
+    it("should populate the scope with available languages", function() {
         expect(this.scope.langs).not.toBeNull();
-    }));
+    });
 
     it("should set the default language", function() {
        expect(this.scope.optlang).toEqual(this.scope.langs[0]);
     });
 
     it("should change the default language", function() {
-        this.scope.changeLang({"name" : "Français", "code":"fr"});
-        expect(this.scope.optlang).toEqual({"name" : "Français", "code":"fr"});
+        this.scope.changeLang(this.scope.langs[1]);
+        expect(this.translate.uses).toHaveBeenCalledWith("fr");
     });
-}));
+});
