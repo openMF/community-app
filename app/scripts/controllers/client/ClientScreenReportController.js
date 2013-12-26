f4f381cb6209e96e2fe84c6f24412b3f167a7a37
Safiyu <dheensafiyu@gmail.com>
2013-12-26 17:39:51 +0530

    fixed issue #661
    bug

--- app/scripts/controllers/client/ClientScreenReportController.js
+++ app/scripts/controllers/client/ClientScreenReportController.js
@@ -4,7 +4,16 @@
         resourceFactory.templateResource.get({entityId : 0, typeId : 0}, function(data) {
             scope.clientTemplateData = data;
         });
-        
+        scope.print = function(template){
+            var templateWindow = window.open('', 'Screen Report', 'height=400,width=600');
+            templateWindow.document.write('<html><head>');
+            templateWindow.document.write('</head><body>');
+            templateWindow.document.write(template);
+            templateWindow.document.write('</body></html>');
+            templateWindow.print();
+            templateWindow.close();
+        };
+
         scope.getClientTemplate = function(templateId) {
           scope.selectedTemplate = templateId;
           $http({
