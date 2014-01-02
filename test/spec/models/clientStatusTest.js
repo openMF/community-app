772e7c96b2402bfea0f2ddc6368ed26847133cb4
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2014-01-02 11:34:13 -0700

    added unit tests for clientStatus model

--- test/spec/models/clientStatusTest.js
+++ test/spec/models/clientStatusTest.js
@@ -1,25 +1,33 @@
 describe("clientStatus", function() {
 
     it ("should assign the correct information for a pending status", function() {
-        var clientStatus = mifos.models.clientStatus("Pending");
+        var clientStatus = new mifosX.models.clientStatus("Pending");
+        var pendingStatus = clientStatus.statusTypes["Pending"];
 
-        expect(clientStatus.getStatus())
+        expect(clientStatus.getStatus()).toEqual(pendingStatus);
 
     });
 
     it ("should assign the correct information for an active status", function() {
-        var clientStatus = mifos.models.clientStatus("Active");
+        var clientStatus = new mifosX.models.clientStatus("Active");
+        var activeStatus = clientStatus.statusTypes["Active"];
+
+        expect(clientStatus.getStatus()).toEqual(activeStatus);
 
     });
 
     it ("should assign the correct information for a status that is a transfer in progress", function() {
-        var clientStatus = mifos.models.clientStatus("Transfer in progress");
+        var clientStatus = new mifosX.models.clientStatus("Transfer in progress");
+        var transferInProgressStatus = clientStatus.statusTypes["Transfer in progress"];
+
+        expect(clientStatus.getStatus()).toEqual(transferInProgressStatus);
 
     });
 
     it ("should assign the correct information for a status that is a transfer on hold", function() {
+        var clientStatus = new mifosX.models.clientStatus("Transfer on hold");
+        var transferOnHoldStatus = clientStatus.statusTypes["Transfer on hold"];
 
-        var clientStatus = mifos.models.clientStatus("Transfer on hold");
-
+        expect(clientStatus.getStatus()).toEqual(transferOnHoldStatus);
     });
 });
