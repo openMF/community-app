7a5e40fa5ce1fe853105dc88341a1afbb8fbbad0
goutham-M <goutham@confluxtechnologies.com>
2014-02-12 17:55:20 +0530

    holiday tree feature

--- app/scripts/controllers/organization/AddHolController.js
+++ app/scripts/controllers/organization/AddHolController.js
@@ -7,9 +7,92 @@
             scope.date.first = new Date();
             scope.date.second = new Date();
             scope.date.third = new Date();
-            resourceFactory.officeResource.getAllOffices(function(data) {
-                scope.offices = data;
+            var idToNodeMap = {};
+            var holidayOfficeIdArray = [];
+
+            scope.deepCopy = function (obj) {
+                if (Object.prototype.toString.call(obj) === '[object Array]') {
+                  var out = [], i = 0, len = obj.length;
+                  for ( ; i < len; i++ ) {
+                    out[i] = arguments.callee(obj[i]);
+                  }
+                  return out;
+                }
+                if (typeof obj === 'object') {
+                  var out = {}, i;
+                  for ( i in obj ) {
+                    out[i] = arguments.callee(obj[i]);
+                  }
+                  return out;
+                }
+                return obj;
+            }
+
+            resourceFactory.officeResource.getAllOffices(function(data){
+                scope.offices = scope.deepCopy(data);
+                for(var i in data){
+                  data[i].children = [];
+                  idToNodeMap[data[i].id] = data[i];
+                }
+                function sortByParentId(a, b){
+                  return a.parentId - b.parentId;
+                }
+                data.sort(sortByParentId);
+
+                var root = [];
+                for(var i = 0; i < data.length; i++) {
+                  var currentObj = data[i];
+                  if(currentObj.children){
+                      currentObj.collapsed = "true";
+                  }
+                  if(typeof currentObj.parentId === "undefined") {
+                        root.push(currentObj);        
+                  } else {
+                        parentNode = idToNodeMap[currentObj.parentId];
+                        parentNode.children.push(currentObj);
+                  }
+                }
+                scope.treedata = root;
             });
+
+             scope.holidayApplyToOffice = function (node) {
+                if (node.selectedCheckBox === 'true') {
+                    recurHolidayApplyToOffice(node);
+                    holidayOfficeIdArray = _.uniq(holidayOfficeIdArray);
+                } else {
+                    node.selectedCheckBox = 'false';
+                    recurRemoveHolidayAppliedOOffice(node);
+
+                }
+            };
+
+            function recurHolidayApplyToOffice (node) {
+                node.selectedCheckBox = 'true';
+                holidayOfficeIdArray.push(node.id);
+                if (node.children.length > 0) {
+                    for(var i = 0; i < node.children.length; i++) {
+                        node.children[i].selectedCheckBox = 'true';
+                        holidayOfficeIdArray.push(node.children[i].id);
+                        if (node.children[i].children.length > 0) {
+                            recurHolidayApplyToOffice(node.children[i]);
+                        }
+                    }
+                }
+            }
+
+            function recurRemoveHolidayAppliedOOffice (node) {
+                holidayOfficeIdArray = _.without(holidayOfficeIdArray, node.id);
+                if (node.children.length > 0) {
+                    for (var i = 0; i < node.children.length; i++) {
+                        node.children[i].selectedCheckBox = 'false';
+                        holidayOfficeIdArray = _.without(holidayOfficeIdArray, node.children[i].id);
+                        if (node.children[i].children.length > 0) {
+                            recurRemoveHolidayAppliedOOffice(node.children[i]);
+                        }
+                    }
+                }
+            }
+
             scope.minDat = new Date();
             scope.submit = function() {
                 var reqFirstDate = dateFilter(scope.date.first,scope.df);
@@ -24,9 +107,9 @@
                 newholiday.repaymentsRescheduledTo = reqThirdDate;
                 newholiday.description = this.formData.description;
                 newholiday.offices = [];
-                for (var i in this.formData.officeId) {
+                for (var i in holidayOfficeIdArray) {
                     var temp = new Object();
-                    temp.officeId = this.formData.officeId[i];
+                    temp.officeId = holidayOfficeIdArray[i];
                     newholiday.offices.push(temp);
                 }
                 resourceFactory.holValueResource.save(newholiday,function(data){
--- app/scripts/directives/TreeviewDirective.js
+++ app/scripts/directives/TreeviewDirective.js
@@ -9,16 +9,31 @@
                         var nodeId = attrs.nodeId || 'id';
                         var nodeLabel = attrs.nodeLabel || 'label';
                         var nodeChildren = attrs.nodeChildren || 'children';
-                        var template =
-                        '<ul>' +
-                            '<li data-ng-repeat="node in ' + treeModel + '">' +
-                            '<i class="collapsed" data-ng-show="node.' + nodeChildren + '.length && node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
-                            '<i class="expanded" data-ng-show="node.' + nodeChildren + '.length && !node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
-                            '<i class="normal" data-ng-hide="node.' + nodeChildren + '.length"></i> ' +
-                            '<span data-ng-class="node.selected" data-ng-click="' + treeId + '.selectNodeLabel(node)">{{node.' + nodeLabel + '}}</span>' +
-                            '<div data-ng-hide="node.collapsed"  data-tree-id="'+treeId+'" data-tree-model="node.' + nodeChildren + '" data-node-id="' + nodeId +'" data-node-label="' + nodeLabel + '" data-node-children="' + nodeChildren + '"></div>' +
-                            '</li>' +
-                            '</ul>';
+                        var template = "";
+                        if (treeId === "holidaytreeview") {
+                            template =
+                                '<ul>' +
+                                '<li data-ng-repeat="node in ' + treeModel + '">' +
+                                '<input ng-model="node.selectedCheckBox" ng-true-value="true" ng-false-value="false" type="checkbox" data-ng-click="holidayApplyToOffice(node)"></input>'+
+                                '<i class="collapsed" data-ng-show="node.' + nodeChildren + '.length && node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
+                                '<i class="expanded" data-ng-show="node.' + nodeChildren + '.length && !node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
+                                '<i class="normal" data-ng-hide="node.' + nodeChildren + '.length"></i> ' +
+                                '<span data-ng-class="node.selected" data-ng-click="' + treeId + '.selectNodeLabel(node)">{{node.' + nodeLabel + '}}</span>' +
+                                '<div data-ng-hide="node.collapsed"  data-tree-id="'+treeId+'" data-tree-model="node.' + nodeChildren + '" data-node-id="' + nodeId +'" data-node-label="' + nodeLabel + '" data-node-children="' + nodeChildren + '"></div>' +
+                                '</li>' +
+                                '</ul>';
+                        } else {
+                            template =
+                            '<ul>' +
+                                '<li data-ng-repeat="node in ' + treeModel + '">' +
+                                '<i class="collapsed" data-ng-show="node.' + nodeChildren + '.length && node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
+                                '<i class="expanded" data-ng-show="node.' + nodeChildren + '.length && !node.collapsed" data-ng-click="' + treeId + '.selectNodeHead(node)"></i>' +
+                                '<i class="normal" data-ng-hide="node.' + nodeChildren + '.length"></i> ' +
+                                '<span data-ng-class="node.selected" data-ng-click="' + treeId + '.selectNodeLabel(node)">{{node.' + nodeLabel + '}}</span>' +
+                                '<div data-ng-hide="node.collapsed"  data-tree-id="'+treeId+'" data-tree-model="node.' + nodeChildren + '" data-node-id="' + nodeId +'" data-node-label="' + nodeLabel + '" data-node-children="' + nodeChildren + '"></div>' +
+                                '</li>' +
+                                '</ul>';
+                        }
 
                         if( treeId && treeModel ) {
 
