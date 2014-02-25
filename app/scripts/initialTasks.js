aa245d5cdb46e28f18c38c2f039afb5ef5b1eedf
Vishwas Babu A J <vishwas@confluxtechnologies.com>
2014-02-24 18:15:59 -0800

    fix for login failure on demo server

--- app/scripts/initialTasks.js
+++ app/scripts/initialTasks.js
@@ -1,5 +1,5 @@
-(function(mifosX) {
-    var defineHeaders = function($httpProvider , $translateProvider, ResourceFactoryProvider ,HttpServiceProvider) {
+(function (mifosX) {
+    var defineHeaders = function ($httpProvider, $translateProvider, ResourceFactoryProvider, HttpServiceProvider) {
         var mainLink = getLocation(window.location.href);
         var baseApiUrl = "https://demo.openmf.org";
         var host = "";
@@ -7,39 +7,39 @@
         //accessing from openmf server
         if (mainLink.hostname.indexOf('openmf.org') >= 0) {
             var hostname = window.location.hostname;
-            console.log('hostname---'+hostname);
+            console.log('hostname---' + hostname);
             domains = hostname.split('.');
-            console.log('domains---'+domains);
+            console.log('domains---' + domains);
             // For multi tenant hosting
             if (domains[0] == "demo") {
                 $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = 'default';
                 ResourceFactoryProvider.setTenantIdenetifier('default');
-                console.log("demo server",domains[0]);
+                console.log("demo server", domains[0]);
             } else {
                 $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = domains[0];
                 ResourceFactoryProvider.setTenantIdenetifier(domains[0]);
                 console.log("other than demo server", domains[0]);
 
             }
-            host = mainLink.hostname;
-            console.log('hostname from mainLink = ',host);
-        } 
+            host = "https://" + mainLink.hostname;
+            console.log('hostname from mainLink = ', host);
+        }
         //accessing from a file system or other servers
         else {
             if (mainLink.hostname != "") {
-                baseApiUrl = "https://"+mainLink.hostname+(mainLink.port ? ':'+mainLink.port : '');
+                baseApiUrl = "https://" + mainLink.hostname + (mainLink.port ? ':' + mainLink.port : '');
             }
-            
-            if (QueryParameters["baseApiUrl"]) { 
+
+            if (QueryParameters["baseApiUrl"]) {
                 baseApiUrl = QueryParameters["baseApiUrl"];
             }
             var queryLink = getLocation(baseApiUrl);
-            host = "https://"+queryLink.hostname+(queryLink.port ? ':'+queryLink.port : '');
+            host = "https://" + queryLink.hostname + (queryLink.port ? ':' + queryLink.port : '');
             portNumber = queryLink.port;
 
             $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = 'default';
             ResourceFactoryProvider.setTenantIdenetifier('default');
-            if(QueryParameters["tenantIdentifier"]) {
+            if (QueryParameters["tenantIdentifier"]) {
                 $httpProvider.defaults.headers.common['X-Mifos-Platform-TenantId'] = QueryParameters["tenantIdentifier"];
                 ResourceFactoryProvider.setTenantIdenetifier(QueryParameters["tenantIdentifier"]);
             }
@@ -47,15 +47,16 @@
 
         // Angularjs strips everything after colon(:) so we are appending port number again
         // follow this link for more info https://github.com/angular/angular.js/issues/1243 
-        if (portNumber && portNumber != "") {              
-            host = host.concat(':'+portNumber);
-        };
+        if (portNumber && portNumber != "") {
+            host = host.concat(':' + portNumber);
+        }
+        ;
         ResourceFactoryProvider.setBaseUrl(host);
-        HttpServiceProvider.addRequestInterceptor('demoUrl', function(config) {
-            if (portNumber && portNumber != ""){
-                host = host.replace(':'+portNumber, "");
+        HttpServiceProvider.addRequestInterceptor('demoUrl', function (config) {
+            if (portNumber && portNumber != "") {
+                host = host.replace(':' + portNumber, "");
             }
-          return _.extend(config, {url: host + config.url });
+            return _.extend(config, {url: host + config.url });
         });
 
         // Enable CORS! (see e.g. http://enable-cors.org/)
@@ -66,37 +67,35 @@
         $httpProvider.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
 
         // Configure i18n and preffer language
-              //$translateProvider.translations('en', translationsEN);
-              //$translateProvider.translations('de', translationsDE);
+        //$translateProvider.translations('en', translationsEN);
+        //$translateProvider.translations('de', translationsDE);
 
         $translateProvider.useStaticFilesLoader({
-              prefix: 'global-translations/locale-',
-              suffix: '.json'
+            prefix: 'global-translations/locale-',
+            suffix: '.json'
         });
 
         $translateProvider.preferredLanguage('en');
         $translateProvider.fallbackLanguage('en');
 
-  };
-  mifosX.ng.application.config(defineHeaders).run(function($log) {
-    $log.info("Initial tasks are done!");
-  });
+    };
+    mifosX.ng.application.config(defineHeaders).run(function ($log) {
+        $log.info("Initial tasks are done!");
+    });
 }(mifosX || {}));
 
-getLocation = function(href) {
+getLocation = function (href) {
     var l = document.createElement("a");
     l.href = href;
     return l;
 };
 
-QueryParameters = (function() {
+QueryParameters = (function () {
     var result = {};
-    if (window.location.search)
-    {
+    if (window.location.search) {
         // split up the query string and store in an associative array
         var params = window.location.search.slice(1).split("&");
-        for (var i = 0; i < params.length; i++)
-        {
+        for (var i = 0; i < params.length; i++) {
             var tmp = params[i].split("=");
             result[tmp[0]] = unescape(tmp[1]);
         }
