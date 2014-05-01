d86f9f22db0ddd5b18e42e1fb4085ce20152f147
pd <simimeowz@gmail.com>
2014-05-01 15:57:02 -0400

    angular wizard

--- app/scripts/mifosX.js
+++ app/scripts/mifosX.js
@@ -2,7 +2,7 @@ var mifosX = (function (module) {
     module.ng = {
         config: angular.module('config_params', ['configurations']),
         services: angular.module('MifosX_Services', ['ngResource']),
-        application: angular.module('MifosX_Application', ['MifosX_Services', 'config_params', 'webStorageModule', 'ui.bootstrap' , 'pascalprecht.translate', 'nvd3ChartDirectives', 'notificationWidget', 'angularFileUpload', 'modified.datepicker', 'ngRoute', 'ngSanitize', 'LocalStorageModule', 'ngIdle', 'ngCsv', 'frAngular'])
+        application: angular.module('MifosX_Application', ['MifosX_Services', 'config_params', 'webStorageModule', 'ui.bootstrap' , 'pascalprecht.translate', 'nvd3ChartDirectives', 'notificationWidget', 'angularFileUpload', 'modified.datepicker', 'ngRoute', 'ngSanitize', 'LocalStorageModule', 'ngIdle', 'ngCsv', 'frAngular', 'mgo-angular-wizard'])
     };
     return module;
 }(mifosX || {}));
