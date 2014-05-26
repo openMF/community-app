d1f1fb25b861e7a121e6a250fdd00be8fcde08de
Madhukar <madhukar@confluxtechnologies.com>
2014-05-26 14:15:29 +0530

    Fixing comments of #925 pull

--- app/scripts/filters/TranslateDataTableColumn.js
+++ app/scripts/filters/TranslateDataTableColumn.js
@@ -1,36 +1,25 @@
 (function (module) {
     mifosX.filters = _.extend(module, {
-        TranslateDataTableColumn: function () {
+        prettifyDataTableColumn: function () {
+            var prettifyColumnName = function (input, split) {
+                var temp = input.split(split);
+                if (temp[1] && temp[1] != "") {
+                    return temp[1];
+                } else {
+                    return temp[0];
+                }
+            }
+
             return function (input) {
                 var retVal;
                 if (input.indexOf("_cd_") > 0) {
-                    var temp = input.split("_cd_");
-                    if (temp[1] && temp[1] != "") {
-                        retVal = temp[1];
-                    } else {
-                        retVal = temp[0];
-                    }
+                    retVal = prettifyColumnName(input, "_cd_");
                 } else if (input.indexOf("_cv_") > 0) {
-                    var temp = input.split("_cv_");
-                    if (temp[1] && temp[1] != "") {
-                        retVal = temp[1];
-                    } else {
-                        retVal = temp[0];
-                    }
+                    retVal = prettifyColumnName(input, "_cv_");
                 } else if (input.indexOf("_cd") > 0) {
-                    var temp = input.split("_cd");
-                    if (temp[1] && temp[1] != "") {
-                        retVal = temp[1];
-                    } else {
-                        retVal = temp[0];
-                    }
+                    retVal = prettifyColumnName(input, "_cd");
                 } else if (input.indexOf("_cv") > 0) {
-                    var temp = input.split("_cv");
-                    if (temp[1] && temp[1] != "") {
-                        retVal = temp[1];
-                    } else {
-                        retVal = temp[0];
-                    }
+                    retVal = prettifyColumnName(input, "_cv");
                 } else {
                     retVal = input;
                 }
@@ -38,7 +27,7 @@
             }
         }
     });
-    mifosX.ng.application.filter('TranslateDataTableColumn', ['dateFilter', mifosX.filters.TranslateDataTableColumn]).run(function ($log) {
-        $log.info("TranslateDataTableColumn filter initialized");
+    mifosX.ng.application.filter('prettifyDataTableColumn', ['dateFilter', mifosX.filters.prettifyDataTableColumn]).run(function ($log) {
+        $log.info("PrettifyDataTableColumn filter initialized");
     });
 }(mifosX.filters || {}));
