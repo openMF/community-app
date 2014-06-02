01a2fde9790d90fb9fd3c026e93c744975403454
Gaurav Saini <gauravsaini03@gmail.com>
2014-06-02 13:37:14 +0530

    changes to grid and better use of right spacing

--- app/scripts/controllers/savings/ViewSavingsTransactionController.js
+++ app/scripts/controllers/savings/ViewSavingsTransactionController.js
@@ -1,6 +1,6 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ViewSavingsTransactionController: function (scope, resourceFactory, location, routeParams, dateFilter) {
+        ViewSavingsTransactionController: function (scope, resourceFactory, location, routeParams, dateFilter, $modal) {
             scope.flag = false;
             resourceFactory.savingsTrxnsResource.get({savingsId: routeParams.accountId, transactionId: routeParams.id}, function (data) {
                 scope.transaction = data;
@@ -8,18 +8,39 @@
                     scope.flag = true;
                 }
             });
-
-            scope.undoTransaction = function (accountId, transactionId) {
-                var params = {savingsId: accountId, transactionId: transactionId, command: 'undo'};
-                var formData = {dateFormat: scope.df, locale: scope.optlang.code, transactionAmount: 0};
-                formData.transactionDate = dateFilter(new Date(), scope.df);
-                resourceFactory.savingsTrxnsResource.save(params, formData, function (data) {
-                    location.path('/viewsavingaccount/' + data.savingsId);
+            
+            scope.undo = function (accountId, transactionId) {
+                $modal.open({
+                    templateUrl: 'undotransaction.html',
+                    controller: UndoTransactionModel,
+                    resolve: {
+                        accountId: function () {
+                          return accountId;
+                        },
+                        transactionId: function () {
+                          return transactionId;
+                        }
+                    }
                 });
+            };
+            
+            var UndoTransactionModel = function ($scope, $modalInstance, accountId, transactionId) {
+                $scope.undoTransaction = function () {
+                    var params = {savingsId: accountId, transactionId: transactionId, command: 'undo'};
+                    var formData = {dateFormat: scope.df, locale: scope.optlang.code, transactionAmount: 0};
+                    formData.transactionDate = dateFilter(new Date(), scope.df);
+                    resourceFactory.savingsTrxnsResource.save(params, formData, function (data) {
+                        $modalInstance.close('delete');
+                        location.path('/viewsavingaccount/' + data.savingsId);
+                    });
+                };
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
             };
         }
     });
-    mifosX.ng.application.controller('ViewSavingsTransactionController', ['$scope', 'ResourceFactory', '$location', '$routeParams', 'dateFilter', mifosX.controllers.ViewSavingsTransactionController]).run(function ($log) {
+    mifosX.ng.application.controller('ViewSavingsTransactionController', ['$scope', 'ResourceFactory', '$location', '$routeParams', 'dateFilter', '$modal', mifosX.controllers.ViewSavingsTransactionController]).run(function ($log) {
         $log.info("ViewSavingsTransactionController initialized");
     });
 }(mifosX.controllers || {}));
