0cba1ba7fe0e6852035f998e2be3663ae7dbb04e
Gaurav Saini <gauravsaini03@gmail.com>
2014-08-08 02:42:31 +0530

    Improvement in groups and manage groups redesigned.

--- app/scripts/controllers/groups/MemberManageController.js
+++ app/scripts/controllers/groups/MemberManageController.js
@@ -2,8 +2,13 @@
     mifosX.controllers = _.extend(module, {
         MemberManageController: function (scope, routeParams, route, location, resourceFactory) {
             scope.group = [];
-            scope.managecode = routeParams.managecode;
+            scope.addedClients = [];
+            scope.formData = {};
 
+            scope.viewClient = function (item) {
+                scope.client = item;
+            };
+            
             resourceFactory.groupResource.get({groupId: routeParams.id, associations: 'all'}, function (data) {
                 scope.group = data;
             });
@@ -13,19 +18,49 @@
                 scope.allMembers = data.clientMembers;
             });
 
-            scope.associate = function () {
-                resourceFactory.groupResource.save({groupId: routeParams.id, command: 'associateClients'}, this.formData, function (data) {
-                    location.path('/viewgroup/' + data.groupId);
-                });
+            
+            scope.add = function () {
+                if(scope.available != ""){
+                    var temp = {};
+                    temp.id = scope.available.id;
+                    temp.displayName = scope.available.displayName;
+                    scope.addedClients.push(temp);
+                }
             };
 
-            scope.disassociate = function () {
-                var disassociateMembers = new Object();
-                disassociateMembers.clientMembers = this.formData.clientMembers;
-                resourceFactory.groupResource.save({groupId: routeParams.id, command: 'disassociateClients'}, disassociateMembers, function (data) {
-                    location.path('/viewgroup/' + data.groupId);
+            scope.sub = function (id) {
+                for (var i = 0; i < scope.addedClients.length; i++) {
+                    if (scope.addedClients[i].id == id) {
+                        scope.addedClients.splice(i, 1);
+                        break;
+                    }
+                }
+            };
+            
+            scope.remove = function (id) {
+            	scope.disassociate = {};
+            	scope.disassociate.clientMembers = [];
+            	scope.disassociate.clientMembers.push(id);
+            	console.log(scope.disassociate);
+            	resourceFactory.groupResource.save({groupId: routeParams.id, command: 'disassociateClients'}, scope.disassociate, function (data) {
+                    scope.allMembers.splice(0, 1);
                 });
             };
+            
+            scope.submit = function () {
+                scope.formData.clientMembers = [];
+                if(scope.addedClients.length > 0){
+                	for (var i in scope.addedClients) {
+                        scope.formData.clientMembers[i] = scope.addedClients[i].id;
+                    }
+                    resourceFactory.groupResource.save({groupId: routeParams.id, command: 'associateClients'}, scope.formData, function (data) {
+                        location.path('/viewgroup/' + scope.group.id);
+                    });
+                } else {
+                	location.path('/viewgroup/' + scope.group.id);
+				}
+            };
+            
         }
     });
     mifosX.ng.application.controller('MemberManageController', ['$scope', '$routeParams', '$route', '$location', 'ResourceFactory', mifosX.controllers.MemberManageController]).run(function ($log) {
