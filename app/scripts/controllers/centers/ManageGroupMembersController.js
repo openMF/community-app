da0ca89f5c3167edaa25dac8c4e8ed3ba9847580
Gaurav Saini <gauravsaini03@gmail.com>
2014-08-11 10:23:08 +0530

    Center changes and New manage groups (API Pending)

--- app/scripts/controllers/centers/ManageGroupMembersController.js
+++ app/scripts/controllers/centers/ManageGroupMembersController.js
@@ -1,66 +1,61 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ManageGroupMembersController: function (scope, resourceFactory, location, routeParams) {
-
-//            if(routeParams.command == 'associateGroups' || routeParams.command == 'disassociateGroups'){
-//                scope.command = routeParams.command;
-//                scope.taskPermissionName = routeParams.command.toUpperCase()+'_CENTER';
-//            }
-            scope.centerId = routeParams.id;
+        ManageGroupMembersController: function (scope, resourceFactory, location, routeParams, $modal) {
+        	
+        	scope.group = [];
+        	scope.centerId = routeParams.id;
             scope.addedGroups = [];
             scope.formData = {};
-
+        	
+        	scope.viewGroup = function (item) {
+                scope.group = item;
+            };
+            
             resourceFactory.centerResource.get({centerId: routeParams.id, template: 'true', associations: 'groupMembers'}, function (data) {
                 scope.data = data;
                 scope.groupsOptions = data.groupMembersOptions || [];
                 scope.groups = data.groupMembers || [];
             });
-
-            scope.viewGroup = function (item) {
-                scope.group = item;
-            };
-
-            scope.add = function () {
-                if(scope.available != ""){
-                    var temp = {};
-                    temp.id = scope.available.id;
-                    temp.name = scope.available.name;
-                    scope.addedGroups.push(temp);
-                }
-            };
-
-            scope.sub = function (id) {
-                for (var i = 0; i < scope.addedGroups.length; i++) {
-                    if (scope.addedGroups[i].id == id) {
-                        scope.addedGroups.splice(i, 1);
-                        break;
-                    }
-                }
-            };
             
+            scope.add = function () {
+            	if(scope.available != ""){
+	                scope.associate = {};
+	            	scope.associate.groupMembers = [];
+	                scope.associate.groupMembers[0] = scope.available.id;
+	                console.log(scope.associate);
+	                var temp = {};
+                    temp.id = scope.available.id;
+                    temp.displayName = scope.available.displayName;
+	                resourceFactory.centerResource.save({centerId: routeParams.id, command: 'associateGroups' }, scope.associate, function (data) {
+	                	scope.groups.push(temp);
+	                });
+            	}
+            };
+
             scope.remove = function (id) {
+            	$modal.open({
+                    templateUrl: 'delete.html',
+                    controller: GroupDeleteCtrl
+                });
             	scope.disassociate = {};
             	scope.disassociate.groupMembers = [];
             	scope.disassociate.groupMembers.push(id);
-            	console.log(scope.disassociate);
-            	resourceFactory.centerResource.save({centerId: routeParams.id, command: 'disassociateGroups' }, scope.disassociate, function (data) {
-            		scope.addedGroups.splice(i, 1);
-            	});
             };
-
-            scope.submit = function () {
-                scope.formData.groupMembers = [];
-                for (var i in scope.addedGroups) {
-                    scope.formData.groupMembers[i] = scope.addedGroups[i].id;
-                }
-
-                resourceFactory.centerResource.save({centerId: routeParams.id, command: 'associateGroups' }, scope.formData, function (data) {
-                    location.path('/viewcenter/' + data.resourceId);
-                });
+            
+            var GroupDeleteCtrl = function ($scope, $modalInstance) {
+                $scope.delete = function () {
+                	resourceFactory.centerResource.save({centerId: routeParams.id, command: 'disassociateGroups' }, scope.disassociate, function (data) {
+                		scope.groups.splice(i, 1);
+                        $modalInstance.close('activate');
+                	});
+                };
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
             };
         }
     });
-    mifosX.ng.application.controller('ManageGroupMembersController', ['$scope', 'ResourceFactory', '$location', '$routeParams', mifosX.controllers.ManageGroupMembersController]).run(function ($log) {
+    mifosX.ng.application.controller('ManageGroupMembersController', ['$scope', 'ResourceFactory', '$location', '$routeParams', '$modal', mifosX.controllers.ManageGroupMembersController]).run(function ($log) {
         $log.info("ManageGroupMembersController initialized");
     });
 }(mifosX.controllers || {}));
