137bc146fe3d0834458a1353e1e99d4db0a7f7ef
Shashvat Tripathi <shashvattrip@gmail.com>
2014-12-25 18:12:29 +0530

    MIFOSX-1617 - Better Pagination
    Better Pagination of Clients listing using
    https://github.com/michaelbromley/angularUtils/tree/master/src/directives/pagination#working-with-asynchronous-data
    
    I have simply commented out the earlier pager implementation.

--- app/scripts/controllers/client/ClientController.js
+++ app/scripts/controllers/client/ClientController.js
@@ -1,12 +1,13 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ClientController: function (scope, resourceFactory, paginatorService, location) {
+        ClientController: function (scope, resourceFactory, location) {
             scope.clients = [];
 
             scope.routeTo = function (id) {
                 location.path('/viewclient/' + id);
             };
 
+            scope.clientsPerPage = 15;
             /* -----Throws error on test-----
              if (!scope.searchCriteria.clients) {
              scope.searchCriteria.clients = null;
@@ -19,14 +20,27 @@
              scope.saveSC();
              };*/
 
-            var fetchFunction = function (offset, limit, callback) {
-                resourceFactory.clientResource.getAllClients({offset: offset, limit: limit}, callback);
-            };
+            // var fetchFunction = function (offset, limit, callback) {
+            //     resourceFactory.clientResource.getAllClients({offset: offset, limit: limit}, callback);
+            // };
 
-            scope.clients = paginatorService.paginate(fetchFunction, 14);
+            // scope.clients = paginatorService.paginate(fetchFunction, 14);
+
+            scope.getResultsPage = function(pageNumber) {
+                var items = resourceFactory.clientResource.getAllClients({offset:((pageNumber - 1) * scope.clientsPerPage), limit:scope.clientsPerPage}, function(data) {
+                    scope.clients = data.pageItems;
+                });
+            }
+            scope.initPage = function() {
+                var items = resourceFactory.clientResource.getAllClients({offset:0, limit: scope.clientsPerPage}, function(data) {
+                    scope.totalClients = data.totalFilteredRecords;
+                    scope.clients = data.pageItems;
+                });
+            }
+            scope.initPage();
         }
     });
-    mifosX.ng.application.controller('ClientController', ['$scope', 'ResourceFactory', 'PaginatorService', '$location', mifosX.controllers.ClientController]).run(function ($log) {
+    mifosX.ng.application.controller('ClientController', ['$scope', 'ResourceFactory', '$location', mifosX.controllers.ClientController]).run(function ($log) {
         $log.info("ClientController initialized");
     });
 }(mifosX.controllers || {}));
