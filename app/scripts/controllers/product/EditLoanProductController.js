75ea8258e45374b1068a8d3980b6c3b6c331be11
Pramod Nuthakki <pramod@confluxtechnologies.com>
2014-12-31 18:19:43 +0530

    MIFOSX-1613 : UI changes for npa and overdue status

--- app/scripts/controllers/product/EditLoanProductController.js
+++ app/scripts/controllers/product/EditLoanProductController.js
@@ -70,6 +70,7 @@
                     graceOnInterestCharged: scope.product.graceOnInterestCharged,
                     graceOnArrearsAgeing: scope.product.graceOnArrearsAgeing,
                     overdueDaysForNPA: scope.product.overdueDaysForNPA,
+                    accountMovesOutOfNPAOnlyOnArrearsCompletion: scope.product.accountMovesOutOfNPAOnlyOnArrearsCompletion,
                     accountingRule: scope.product.accountingRule.id,
                     principalVariationsForBorrowerCycle: [],
                     interestRateVariationsForBorrowerCycle: [],
@@ -89,6 +90,7 @@
                     scope.formData.rescheduleStrategyMethod = scope.product.interestRecalculationData.rescheduleStrategyType.id;
                     scope.formData.recalculationRestFrequencyType = scope.product.interestRecalculationData.recalculationRestFrequencyType.id;
                     scope.formData.recalculationRestFrequencyInterval = scope.product.interestRecalculationData.recalculationRestFrequencyInterval;
+                    scope.formData.isArrearsBasedOnOriginalSchedule = scope.product.interestRecalculationData.isArrearsBasedOnOriginalSchedule;
                     if (scope.product.interestRecalculationData.recalculationRestFrequencyDate) {
                         scope.date.recalculationRestFrequencyDate = new Date(scope.product.interestRecalculationData.recalculationRestFrequencyDate);
                     }
@@ -170,7 +172,7 @@
                     _.each(scope.product.penaltyToIncomeAccountMappings, function (penalty) {
                         scope.penaltySpecificIncomeaccounts.push({
                             chargeId: penalty.charge.id,
-                            incomeAccountId: penalty.incomeAccount.id,
+                            incomeAccountId: penalty.incomeAccount.id
                         })
                     });
                 }
