80e2159cb1db559faee33a27742950be5b4b80b9
Shashvat Tripathi <shashvattrip@gmail.com>
2015-01-23 12:54:02 +0530

    MIFOSX-1617
    List all Groups and Centers Pages paginated.

--- app/scripts/controllers/centers/CenterController.js
+++ app/scripts/controllers/centers/CenterController.js
@@ -1,8 +1,8 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        CenterController: function (scope, resourceFactory, paginatorService, location) {
+        CenterController: function (scope, resourceFactory, location) {
             scope.centers = [];
-
+            scope.centersPerPage = 14;
             scope.routeTo = function (id) {
                 location.path('/viewcenter/' + id);
             };
@@ -18,14 +18,26 @@
                 scope.saveSC();
             };
 
-            var fetchFunction = function (offset, limit, callback) {
-                resourceFactory.centerResource.get({offset: offset, limit: limit, paged: 'true', orderBy: 'name', sortOrder: 'ASC'}, callback);
-            };
+            // var fetchFunction = function (offset, limit, callback) {
+            //     resourceFactory.centerResource.get({offset: offset, limit: limit, paged: 'true', orderBy: 'name', sortOrder: 'ASC'}, callback);
+            // };
 
-            scope.centers = paginatorService.paginate(fetchFunction, 14);
+            // scope.centers = paginatorService.paginate(fetchFunction, 14);
+            scope.getResultsPage = function(pageNumber) {
+                var items = resourceFactory.centerResource.getAllCenters({offset:((pageNumber - 1) * scope.centersPerPage), limit:scope.centersPerPage}, function(data) {
+                    scope.centers = data;
+                });
+            }
+            scope.initPage = function() {
+                var items = resourceFactory.centerResource.get({offset:0, limit: scope.groupsPerPage, orderBy:'name', paged:true, sortOrder:'ASC'}, function(data) {
+                    scope.totalCenters = data.totalFilteredRecords;
+                    scope.centers = data.pageItems;
+                });
+            }
+            scope.initPage();
         }
     });
-    mifosX.ng.application.controller('CenterController', ['$scope', 'ResourceFactory', 'PaginatorService', '$location', mifosX.controllers.CenterController]).run(function ($log) {
+    mifosX.ng.application.controller('CenterController', ['$scope', 'ResourceFactory', '$location', mifosX.controllers.CenterController]).run(function ($log) {
         $log.info("CenterController initialized");
     });
 }(mifosX.controllers || {}));
--- app/scripts/controllers/groups/GroupController.js
+++ app/scripts/controllers/groups/GroupController.js
@@ -1,8 +1,8 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        GroupController: function (scope, resourceFactory, paginatorService, location) {
+        GroupController: function (scope, resourceFactory, location) {
             scope.groups = [];
-
+            scope.groupsPerPage = 14;
             scope.routeTo = function (id) {
                 location.path('/viewgroup/' + id);
             };
@@ -18,17 +18,30 @@
                 scope.saveSC();
             };
 
-            var fetchFunction = function (offset, limit, callback) {
-                resourceFactory.groupResource.get({
-                    offset: offset, limit: limit, paged: 'true',
-                    orderBy: 'name', sortOrder: 'ASC'
-                }, callback);
-            };
+            // var fetchFunction = function (offset, limit, callback) {
+            //     resourceFactory.groupResource.get({
+            //         offset: offset, limit: limit, paged: 'true',
+            //         orderBy: 'name', sortOrder: 'ASC'
+            //     }, callback);
+            // };
 
-            scope.groups = paginatorService.paginate(fetchFunction, 14);
+            // scope.groups = paginatorService.paginate(fetchFunction, 14);
+
+            scope.getResultsPage = function(pageNumber) {
+                var items = resourceFactory.groupResource.getAllGroups({offset:((pageNumber - 1) * scope.groupsPerPage), limit:scope.groupsPerPage}, function(data) {
+                    scope.groups = data;
+                });
+            }
+            scope.initPage = function() {
+                var items = resourceFactory.groupResource.get({offset:0, limit: scope.groupsPerPage, orderBy:'name', paged:true, sortOrder:'ASC'}, function(data) {
+                    scope.totalGroups = data.totalFilteredRecords;
+                    scope.groups = data.pageItems;
+                });
+            }
+            scope.initPage();
         }
     });
-    mifosX.ng.application.controller('GroupController', ['$scope', 'ResourceFactory', 'PaginatorService', '$location', mifosX.controllers.GroupController]).run(function ($log) {
+    mifosX.ng.application.controller('GroupController', ['$scope', 'ResourceFactory', '$location', mifosX.controllers.GroupController]).run(function ($log) {
         $log.info("GroupController initialized");
     });
 }(mifosX.controllers || {}));
