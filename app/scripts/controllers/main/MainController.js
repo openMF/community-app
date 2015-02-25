2f225093e3ea78718fed442323ea86ae9392d3d1
sachinkulkarni12 <sachin.kulkarni@confluxtechnologies.com>
2015-02-25 11:47:25 +0530

    MIFOSX-1888:New feature based configuration file to specify which fields should be editable/read only/hidden

--- app/scripts/controllers/main/MainController.js
+++ app/scripts/controllers/main/MainController.js
@@ -1,8 +1,9 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        MainController: function (scope, location, sessionManager, translate, $rootScope, localStorageService, keyboardManager, $idle, tmhDynamicLocale) {
+        MainController: function (scope, location, sessionManager, translate, $rootScope, localStorageService, keyboardManager, $idle, tmhDynamicLocale,   UIConfigService) {
 			scope.version = "1.26.0";
 			scope.releasedate = "22/Dec/14"; //Day/Month/Year
+            UIConfigService.init();
 			//hides loader
             scope.domReady = true;
             scope.activity = {};
@@ -331,6 +332,7 @@
         'localStorageService',
         'keyboardManager', '$idle',
         'tmhDynamicLocale',
+        'UIConfigService',
         mifosX.controllers.MainController
     ]).run(function ($log) {
         $log.info("MainController initialized");
