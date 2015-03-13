ea19ec968803e0bbb033f3ef1abe718cf841d3c4
Terence Monteiro <terence@sanjosesolutions.in>
2015-03-13 12:32:39 +0400

    Replaced app/VERSION.txt with app/release.json

--- release.sh
+++ release.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 VERSION=$1
+RELDATE=$2
 
-if [ -z $VERSION ]; then
-  echo -e "Version required.\nUsage: $0 version"
+if [ -z $VERSION -o -z $RELDATE ]; then
+  echo -e "Version required.\nUsage: $0 version releasedate"
 fi
 
 echo "Updating version to $VERSION"
-echo $VERSION > app/VERSION.txt
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo -e "{\n  \"version\": \"$VERSION\",\n  \"releasedate\": \"$RELDATE\"\n}\n" > $DIR/app/release.json
 
