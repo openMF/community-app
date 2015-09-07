49b51c10542bc9f584a33d9527d8787779d57a70
Adi Raju <rajuan@gmail.com>
2015-09-07 15:34:43 +0530

    MIFOSX-1746 : Community-app changes to be able to support Oauth2, configurable in modules/configurations.js.

--- app/scripts/services/SessionManager.js
+++ app/scripts/services/SessionManager.js
@@ -1,14 +1,28 @@
 (function (module) {
     mifosX.services = _.extend(module, {
-        SessionManager: function (webStorage, httpService, resourceFactory) {
+        SessionManager: function (webStorage, httpService, SECURITY, resourceFactory, localStorageService) {
             var EMPTY_SESSION = {};
 
             this.get = function (data) {
+                var isOauth = SECURITY === 'oauth';
+		        var accessToken = null;
+                if(isOauth){
+                    accessToken = localStorageService.getFromLocalStorage("tokendetails").access_token;
+                }
                 if (data.shouldRenewPassword) {
-                    httpService.setAuthorization(data.base64EncodedAuthenticationKey);
-                } else{
-                    webStorage.add("sessionData", {userId: data.userId, authenticationKey: data.base64EncodedAuthenticationKey, userPermissions: data.permissions});
-                    httpService.setAuthorization(data.base64EncodedAuthenticationKey);
+                    if(isOauth){
+                        httpService.setAuthorization(data.accessToken);
+                    } else {
+                        httpService.setAuthorization(data.base64EncodedAuthenticationKey);
+                    }
+                } else {
+                    if(isOauth){
+                        webStorage.add("sessionData", {userId: data.userId, authenticationKey: data.accessToken, userPermissions: data.permissions});
+                        httpService.setAuthorization(data.accessToken, true);
+                    } else {
+                        webStorage.add("sessionData", {userId: data.userId, authenticationKey: data.base64EncodedAuthenticationKey, userPermissions: data.permissions});
+                        httpService.setAuthorization(data.base64EncodedAuthenticationKey, false);
+                    }
                     return {user: new mifosX.models.LoggedInUser(data)};
                 };
             }
@@ -36,7 +50,9 @@
     mifosX.ng.services.service('SessionManager', [
             'webStorage',
             'HttpService',
+            'SECURITY',
             'ResourceFactory',
+            'localStorageService',
             mifosX.services.SessionManager
         ]).run(function ($log) {
             $log.info("SessionManager initialized");
