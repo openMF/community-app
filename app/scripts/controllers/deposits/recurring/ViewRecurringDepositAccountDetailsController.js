06b3ec5d02ba9e47ca4f4e9d8c13402efe076807
kamdjou duplex <kamdjouduplex@outlook.com>
2016-03-31 19:33:36 +0100

    intergrated the new UI design as from the issue

--- app/scripts/controllers/deposits/recurring/ViewRecurringDepositAccountDetailsController.js
+++ app/scripts/controllers/deposits/recurring/ViewRecurringDepositAccountDetailsController.js
@@ -81,6 +81,7 @@
 
             resourceFactory.recurringDepositAccountResource.get({accountId: routeParams.id, associations: 'all'}, function (data) {
                 scope.savingaccountdetails = data;
+                console.log(scope.savingaccountdetails);
                 scope.convertDateArrayToObject('date');
                 scope.chartSlabs = scope.savingaccountdetails.accountChart.chartSlabs;
                 scope.isprematureAllowed = data.maturityDate != null;
