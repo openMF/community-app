cd0681e751fabe00636867cb9b630b7e013d39f5
Adi Narayana Raju <adi.raju@confluxtechnologies.com>
2016-05-10 14:18:40 +0530

    [FINERACT-152][FINERACT-153] - Dormant Savings Issues

--- app/scripts/controllers/savings/AddNewSavingsChargeController.js
+++ app/scripts/controllers/savings/AddNewSavingsChargeController.js
@@ -15,7 +15,13 @@
                     scope.chargeTimeType = data.chargeTimeType.id;
                     scope.chargeDetails = data;
                     scope.formData.amount = data.amount;
-                    scope.withDrawCharge = data.chargeTimeType.value === "Withdrawal Fee" ? true : false;
+                    if(data.chargeTimeType.value === "Withdrawal Fee"
+                        || data.chargeTimeType.value === "Saving No Activity Fee"
+                        || data.chargeTimeType.value === "Savings Activation"
+                        || data.chargeTimeType.value === "Savings Closure"
+                        || data.chargeTimeType.value === "Overdraft Fee"){
+                        scope.dueDateNotRequired = true;
+                    }
                     scope.formData.feeInterval = data.feeInterval;
                     if (data.chargeTimeType.value === "Annual Fee" || data.chargeTimeType.value === "Monthly Fee") {
                         scope.chargeTimeTypeAnnualOrMonth = true;
@@ -25,7 +31,7 @@
 
             scope.submit = function () {
                 this.formData.locale = scope.optlang.code;
-                if (scope.withDrawCharge !== true) {
+                if (scope.dueDateNotRequired !== true) {
                     if (scope.chargeTimeTypeAnnualOrMonth === true) {
                         this.formData.monthDayFormat = "dd MMMM";
                         if (scope.date.due) {
