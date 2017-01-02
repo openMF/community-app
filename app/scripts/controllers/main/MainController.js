00b6144f49e7373e26ca94f517dc58b36354b8c8
Mexina M Daniel <mexinmiel@gmail.com>
2017-01-02 17:15:42 +0300

    Corrected the paragraph in login page

--- app/scripts/controllers/main/MainController.js
+++ app/scripts/controllers/main/MainController.js
@@ -196,7 +196,7 @@
 
             };
             scope.text = '<span>Mifos X is designed by the <a href="http://www.openmf.org/">Mifos Initiative</a>.' +
-            '<a href="http://mifos.org/resources/community/"> A global community </a> thats aims to speed the elimination of poverty by enabling Organizations to more effectively and efficiently deliver responsible financial services to the world’s poor and unbanked </span><br/>' +
+            '<a href="http://mifos.org/resources/community/"> A global community </a> that aims to speed the elimination of poverty by enabling Organizations to more effectively and efficiently deliver responsible financial services to the world’s poor and unbanked </span><br/>' +
             '<span>Sounds interesting?<a href="http://mifos.org/take-action/volunteer/"> Get involved!</a></span>';
 
             scope.logout = function () {
