3ef3d9dadbf5f24647d8e2cc7c0be98a27fccf44
mbj36 <mohitbajo36@gmail.com>
2017-03-16 18:13:33 +0530

    Adding dependencies for testing in production

--- app/scripts/loader.js
+++ app/scripts/loader.js
@@ -11,6 +11,8 @@
             'angularui': '../bower_components/angular-bootstrap/ui-bootstrap',
             'angularuitpls': '../bower_components/angular-bootstrap/ui-bootstrap-tpls',
             'bootstrap': '../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min',
+            'chosen': '../bower_components/angular-bootstrap/ui-bootstrap-csp.css',
+            'bootstrap-csp': '../bower_components/angular-bootstrap/ui-bootstrap-csp.css',
             'underscore': '../bower_components/underscore/underscore',
             'webstorage': '../bower_components/angular-webstorage/angular-webstorage',
             'angular-animate': '../bower_components/angular-animate/angular-animate',
@@ -40,7 +42,9 @@
 
             'webcam-directive':'../bower_components/webcam-directive/dist/webcam.min',
             'angular-utils-pagination':'../bower_components/angular-utils-pagination/dirPagination',
-            'ng-scrollbar':'../bower_components/ng-scrollbar/dist/ng-scrollbar'
+            'ng-scrollbar':'../bower_components/ng-scrollbar/dist/ng-scrollbar',
+            'ng-scrollbar-css':'../bower_components/ng-scrollbar/dist/ng-scrollbar.css',
+            'styles': '../styles/styles.css'
         },
         shim: {
             'angular': { deps: ['jquery','chosen.jquery.min'],exports: 'angular' },
