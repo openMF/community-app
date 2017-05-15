5d832acda7f1bb1eb23cc4cf5b3626f778fa0597
nazeer1100126 <nazeer.shaik@confluxtechnologies.com>
2017-05-15 21:02:32 +0530

    450 bulk jlg with ouevrdue charge

--- app/scripts/controllers/loanAccount/NewJLGLoanAccAppController.js
+++ app/scripts/controllers/loanAccount/NewJLGLoanAccAppController.js
@@ -208,8 +208,7 @@
                         loanApplication.charges = [];
 
                         for (var j in scope.group.clients[i].charges) {
-
-                            if(!scope.group.clients[i].charges[j].isDeleted){
+                            if(!scope.group.clients[i].charges[j].isDeleted && scope.group.clients[i].charges[j].chargeTimeType.code !="chargeTimeType.overdueInstallment"){
                                 var charge = {};
                                 charge.amount = scope.group.clients[i].charges[j].amount;
                                 charge.chargeId = scope.group.clients[i].charges[j].id;
@@ -217,6 +216,7 @@
                             }
 
                         }
+
                         if (!_.isUndefined(scope.formData.datatables) && scope.formData.datatables.length > 0) {
                             loanApplication.datatables = scope.formData.datatables;
                         }
