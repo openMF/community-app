41f0112997a0ee14fc77da02def996e36daf937e
Anh3h <courageangeh@gmail.com>
2017-06-02 16:37:03 +0100

    Reskin: Fix #2133 - 'Submit' button is not working on Upload Client Document Page

--- app/scripts/controllers/client/ClientDocumentController.js
+++ app/scripts/controllers/client/ClientDocumentController.js
@@ -2,15 +2,14 @@
     mifosX.controllers = _.extend(module, {
         ClientDocumentController: function (scope, location, http, routeParams, API_VERSION, Upload, $rootScope) {
             scope.clientId = routeParams.clientId;
-            scope.onFileSelect = function ($files) {
-                scope.file = $files[0];
+            scope.onFileSelect = function (files) {
+                scope.formData.file = files[0];
             };
 
             scope.submit = function () {
                 Upload.upload({
                     url: $rootScope.hostUrl + API_VERSION + '/clients/' + scope.clientId + '/documents',
-                    data: scope.formData,
-                    file: scope.file
+                    data: { name : scope.formData.name, description : scope.formData.description, file: scope.formData.file},
                 }).then(function (data) {
                         // to fix IE not refreshing the model
                         if (!scope.$$phase) {
