13774f281dd9720e13ff85e1d29f9bb4227746f5
nazeer shaik <nazeer.shaik@confluxtechnologies.com>
2017-07-20 16:57:25 +0530

    holiday changes

--- app/scripts/controllers/organization/EditHolidayController.js
+++ app/scripts/controllers/organization/EditHolidayController.js
@@ -4,13 +4,24 @@
             scope.formData = {};
             scope.date = {};
             scope.restrictDate = new Date();
+            scope.specificRescheduleType = 2;
 
             resourceFactory.holValueResource.getholvalues({holId: routeParams.id}, function (data) {
                 scope.holiday = data;
+                 resourceFactory.holidayTemplateResource.get(function(repaymentSchedulingRulesData){
+                    scope.repaymentSchedulingRules = repaymentSchedulingRulesData;
+                    for(var i in scope.repaymentSchedulingRules){
+                        if(scope.repaymentSchedulingRules[i].id == data.reschedulingType){
+                            scope.reschedulingType = scope.repaymentSchedulingRules[i]; 
+                        }
+                    }
+                    
+                });
                 scope.formData = {
                     name: data.name,
                     description: data.description,
                 };
+                scope.formData.reschedulingType = data.reschedulingType;
 
                 scope.holidayStatusActive = false;
                 if (data.status.value === "Active") {
@@ -23,10 +34,20 @@
                 var toDate = dateFilter(data.toDate, scope.df);
                 scope.date.toDate = new Date(toDate);
 
-                var repaymentsRescheduledTo = dateFilter(data.repaymentsRescheduledTo, scope.df);
-                scope.date.repaymentsRescheduledTo = new Date(repaymentsRescheduledTo);
+                if(data.reschedulingType == scope.specificRescheduleType){
+                    var repaymentsRescheduledTo = dateFilter(data.repaymentsRescheduledTo, scope.df);
+                    scope.date.repaymentsRescheduledTo = new Date(repaymentsRescheduledTo);
+                }  
 
             });
+
+            scope.selectRescheduleType = function(data){
+                if(data && data.id == scope.specificRescheduleType){
+                    scope.date.repaymentsRescheduledTo = new Date();
+                }else{
+                    scope.date.repaymentsRescheduledTo = undefined;
+                }
+            };
 
             scope.submit = function () {
                 this.formData.locale = scope.optlang.code;
@@ -35,7 +56,13 @@
                     this.formData.fromDate = dateFilter(scope.date.fromDate, scope.df);
                     this.formData.toDate = dateFilter(scope.date.toDate, scope.df);
                 }
-                this.formData.repaymentsRescheduledTo = dateFilter(scope.date.repaymentsRescheduledTo, scope.df);
+
+                if(scope.reschedulingType.id == scope.specificRescheduleType){
+                    this.formData.repaymentsRescheduledTo = dateFilter(scope.date.repaymentsRescheduledTo, scope.df);
+                }
+                var rescheduleId = scope.reschedulingType.id
+                this.formData.reschedulingType = rescheduleId;
+
                 resourceFactory.holValueResource.update({holId: routeParams.id}, this.formData, function (data) {
                     location.path('/viewholiday/' + routeParams.id);
                 });
