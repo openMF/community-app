6069b936ef731945dbdf4e25e0d92c7c3553d644
Mexina M Daniel <mexinmiel@gmail.com>
2017-10-12 11:19:29 +0300

    Fix issue #2175

--- app/scripts/controllers/loanAccount/LoanAccountActionsController.js
+++ app/scripts/controllers/loanAccount/LoanAccountActionsController.js
@@ -143,7 +143,7 @@
                     scope.isTransaction = true;
                     scope.showAmountField = true;
                     scope.taskPermissionName = 'REPAYMENT_LOAN';
-                    scope.action = 'repayment';
+                    scope.action = 'prepayment';
                     break;
                 case "waiveinterest":
                     scope.modelName = 'transactionDate';
