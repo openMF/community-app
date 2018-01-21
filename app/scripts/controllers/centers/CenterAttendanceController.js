666028a1ed5db06049930e91ba4bacd39032789f
ankit01ojha <anoj123anru@gmail.com>
2018-01-21 11:15:46 +0530

    Fix #2847 - Added missing breadcrumb in attendance

--- app/scripts/controllers/centers/CenterAttendanceController.js
+++ app/scripts/controllers/centers/CenterAttendanceController.js
@@ -6,6 +6,7 @@
             scope.formData = {};
             scope.first = {};
             scope.first.date = new Date();
+            scope.centerId = routeParams.centerId;
             resourceFactory.centerResource.get({centerId: routeParams.centerId, associations: 'groupMembers,collectionMeetingCalendar'}, function (data) {
                 scope.center = data;
                 scope.meeting = data.collectionMeetingCalendar;
