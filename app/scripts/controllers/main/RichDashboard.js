da4b4d6149c6d827c9534ed21c24afdc11eb58bc
abhay_1806 <abhay.chawla97@gmail.com>
2018-02-05 21:16:31 +0530

    Fix #2041 Dashboard Charts and Panel Headers

--- app/scripts/controllers/main/RichDashboard.js
+++ app/scripts/controllers/main/RichDashboard.js
@@ -10,7 +10,11 @@
             scope.uniqueArray = [];
             scope.searchParams = [];
             scope.recents = [];
-            scope.dashModel = 'dashboard';
+            scope.dashModel = 'rich-dashboard';
+
+            scope.officeIdDisbursed = 1;
+            scope.officeId = 1;
+            scope.officeIdCollection = 1;
 
             scope.switch = function() {
 	        	location.path('/richdashboard');
@@ -90,7 +94,7 @@
             scope.bOfficeName = 'Head Office';
             scope.chartType = 'Days';
             scope.collectionPieData = [];
-            scope.dashModel = 'rich-dashboard';
+
             scope.switch = function() {
 	        	location.path('/home');
 			}
