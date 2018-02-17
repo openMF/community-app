2aa451bd8ab9150ea53e7ed3a3ac084f854d113a
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-02-17 22:44:22 +0530

    Fixed #2904

--- app/scripts/controllers/organization/RoleController.js
+++ app/scripts/controllers/organization/RoleController.js
@@ -5,6 +5,18 @@
             scope.routeTo = function (id) {
                 location.path('/admin/viewrole/' + id);
             };
+            
+            if (!scope.searchCriteria.roles) {
+                scope.searchCriteria.roles = null;
+                scope.saveSC();
+            }
+            scope.filterText = scope.searchCriteria.roles || '';
+
+            scope.onFilter = function () {
+                scope.searchCriteria.roles = scope.filterText;
+                scope.saveSC();
+            };
+            
             resourceFactory.roleResource.getAllRoles({}, function (data) {
                 scope.roles = data;
             });
