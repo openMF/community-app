5baf430bf32ff49f02afe5f39e711f979216c8b2
Maek Twain <saranshisat@icloud.com>
2020-04-16 20:19:35 +0530

    Fixed Date For Safari (#3196)

--- app/scripts/filters/DateFormat.js
+++ app/scripts/filters/DateFormat.js
@@ -3,7 +3,9 @@
         DateFormat: function (dateFilter, localStorageService) {
             return function (input) {
                 if (input) {
-                    var tDate = new Date(input);
+                    // SAFARI is Bad We fix it
+                    remove = input.toString().split(",");
+                    var tDate = new Date(remove[0], remove[1]-1, remove[2]);
                     return dateFilter(tDate, localStorageService.getFromLocalStorage('dateformat'));
                 }
                 return '';
