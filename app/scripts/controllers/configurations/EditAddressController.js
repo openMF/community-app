5ae7359a152104a0cc1dc639f64cb1c0e26f614b
habile <support@habile.in>
2021-05-04 21:08:45 +0530

    Not able to create the client address due to missing street field in UI #3249

--- app/scripts/controllers/configurations/EditAddressController.js
+++ app/scripts/controllers/configurations/EditAddressController.js
@@ -61,6 +61,11 @@
                     {
                         if(data[i].addressId==addressId)
                         {
+
+                            if(data[i].street&&$scope.street)
+                            {
+                                $scope.formData.street=data[i].street;
+                            }
                             if(data[i].addressLine1&&$scope.addressLine1)
                             {
                                 $scope.formData.addressLine1=data[i].addressLine1;
